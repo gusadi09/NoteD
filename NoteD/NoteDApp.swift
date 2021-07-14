68a3e6e0a9e19763d1d5aab9bcfce217a2e44242
gusadi <gusadi872@gmail.com>
2021-07-14 20:07:50 +0800

    Initial Commit

---
+++ NoteD/NoteDApp.swift
@@ -0,0 +1,20 @@
+//
+//  NoteDApp.swift
+//  NoteD
+//
+//  Created by Gus Adi on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct NoteDApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
